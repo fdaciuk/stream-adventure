c60de93bd731cf2eaee064ea1047b8063c294af2
Fernando Daciuk <f.daciuk@gmail.com>
2015-09-24 23:00:40 -0300

    Add duplexer redux

--- 12-duplexer-redux/program.js
+++ 12-duplexer-redux/program.js
@@ -1,6 +1,19 @@
 'use strict';
 var duplexer2 = require('duplexer2');
+var through = require('through2').obj;
 
 module.exports = function(counter) {
-  
+  var stream = through(write, end);
+  var counts = {};
+
+  function write(chunk, encoding, next) {
+    counts[chunk.country] = (counts[chunk.country] || 0) + 1;
+    next();
+  }
+
+  function end(done) {
+    counter.setCounts(counts);
+    done();
+  }
+  return duplexer2(stream, counter);
 };
